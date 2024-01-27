4b6aa5330f568d2ec469508f156d5e5cc91cf94b
Matt Alton <mattalton97@gmail.com>
2024-01-27 11:48:01 +0000

    Altered input ring styling

--- src/app/_components/form-inputs/password-input.tsx
+++ src/app/_components/form-inputs/password-input.tsx
@@ -16,7 +16,7 @@ function PasswordInput() {
             name="password"
             id="password"
             autoComplete="password"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
@@ -34,7 +34,7 @@ function PasswordInput() {
             name="confirm-password"
             id="confirm-password"
             autoComplete="password"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
--- src/app/_components/form-inputs/username-input.tsx
+++ src/app/_components/form-inputs/username-input.tsx
@@ -15,7 +15,7 @@ function UsernameInput() {
           name="username"
           id="username"
           autoComplete="username"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 sm:text-sm sm:leading-6"
         />
       </div>
     </>
