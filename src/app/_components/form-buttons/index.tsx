9cd62588fa1034109d7a9778339aa34001ed8193
Matt Alton <mattalton97@gmail.com>
2024-01-28 15:10:48 +0000

    Disable submit button when form is pending

--- src/app/_components/form-buttons/index.tsx
+++ src/app/_components/form-buttons/index.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import React from "react";
+import { useFormStatus } from "react-dom";
 
 function FormButtons() {
+  const { pending } = useFormStatus();
+
   return (
     <div className="mt-6 flex items-center justify-end gap-x-6">
       <button
@@ -11,9 +16,10 @@ function FormButtons() {
       </button>
       <button
         type="submit"
-        className="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+        className="rounded-md enabled:bg-gray-600 disabled:bg-gray-400 px-3 py-2 text-sm font-semibold enabled:text-white disabled:text-gray-200 shadow-sm enabled:hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+        disabled={pending}
       >
-        Save
+        Submit
       </button>
     </div>
   );
